be0faed704c0adc44b664706f357047317c341fc
Neha Gupta <nehagupta161995@gmail.com>
2022-06-03 13:12:09 +0530

    handle race condition

--- googlesource/googlesource.go
+++ googlesource/googlesource.go
@@ -21,6 +21,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
+	"sync"
 	"time"
 
 	"cloud.google.com/go/bigquery"
@@ -48,7 +49,7 @@ func (client *client) Client() (*bigquery.Client, error) {
 // checkInitialPos helps in creating the query to fetch data from endpoint
 func (s *Source) checkInitialPos() (firstSync, userDefinedOffset, userDefinedKey bool) {
 	// if its the firstSync no offset is applied
-	if s.position == "" {
+	if s.getPosition() == "" {
 		firstSync = true
 	}
 
@@ -65,12 +66,18 @@ func (s *Source) checkInitialPos() (firstSync, userDefinedOffset, userDefinedKey
 	return
 }
 
+func (s *Source) getPosition() string {
+	s.position.lock.Lock()
+	defer s.position.lock.Unlock()
+	return s.position.positions
+}
+
 // ReadGoogleRow fetches data from endpoint. It creates sdk.record and puts it in response channel
 func (s *Source) ReadGoogleRow(ctx context.Context) (err error) {
 	sdk.Logger(ctx).Trace().Msg("Inside read google row")
 	var userDefinedOffset, userDefinedKey, firstSync bool
 
-	offset := s.position
+	offset := s.getPosition()
 	tableID := s.sourceConfig.Config.TableID
 
 	firstSync, userDefinedOffset, userDefinedKey = s.checkInitialPos()
@@ -217,10 +224,12 @@ func getType(fieldType bigquery.FieldType, offset string) string {
 	}
 }
 
-// writePosition marshal the position
+// writePosition prevents race condition happening while using map inside goroutine
 func (s *Source) writePosition(offset string) (recPosition []byte, err error) {
-	s.position = offset
-	return json.Marshal(&s.position)
+	s.position.lock.Lock()
+	defer s.position.lock.Unlock()
+	s.position.positions = offset
+	return json.Marshal(&s.position.positions)
 }
 
 // getRowIterator sync data for bigquery using bigquery client jobs
@@ -293,9 +302,12 @@ func (s *Source) Next(ctx context.Context) (sdk.Record, error) {
 
 // fetchPos unmarshal position
 func fetchPos(s *Source, pos sdk.Position) {
-	s.position = ""
+	s.position.lock = new(sync.Mutex)
+	s.position.lock.Lock()
+	defer s.position.lock.Unlock()
+	s.position.positions = ""
 
-	err := json.Unmarshal(pos, &s.position)
+	err := json.Unmarshal(pos, &s.position.positions)
 	if err != nil {
 		sdk.Logger(s.ctx).Info().Msg("Could not get position. Will start with offset 0")
 	}
